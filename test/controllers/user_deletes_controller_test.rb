5f59e6ad8debe9614cb5ece0c204257040c971e8
ykpythemind <yukibukiyou@gmail.com>
2024-12-08 21:58:03 +0900

    退会

---
+++ test/controllers/user_deletes_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class UserDeletesControllerTest < ActionDispatch::IntegrationTest
+  test "#show" do
+    login_as(users(:one))
+
+    get "/users/delete"
+    assert_response :success
+  end
+
+  test "退会できる" do
+    login_as(users(:one))
+
+    WebMock.disable_net_connect!
+
+    stub_request(:post, "https://www.googleapis.com/calendar/v3/channels/stop").and_return(status: 200, body: "", headers: { "Content-Type": "application/json" })
+
+    post "/users/delete"
+    assert_response :redirect
+
+    assert { User.find_by(id: users(:one).id) == nil }
+  end
+end
