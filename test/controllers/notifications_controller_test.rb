60846a4a91fea7530590baf7258c127b6f8cefc3
ykpythemind <yukibukiyou@gmail.com>
2025-01-01 23:45:24 +0900

    test

---
+++ test/controllers/notifications_controller_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class NotificationsControllerTest < ActionDispatch::IntegrationTest
+  test "delete notification" do
+    user = users(:one)
+    notification = user.notifications.create!(
+      notification_type: :authorize_needed, message: "カレンダーの同期のために再度ログインが必要です。")
+
+    login_as(user)
+
+    get "/"
+    assert_response :ok
+
+    assert { user.notifications.where(read: false).count == 1 }
+
+    delete "/notifications/#{notification.id}"
+    assert_response :redirect
+
+    assert { user.notifications.where(read: false).count == 0 }
+  end
+end
